75a039d9b3f8e152494cbb10fa62266569b42567
shepherrrd <shefkid2@gmail.com>
2026-01-06 22:37:13 +0100

    analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/lib/contexts/AuthContext";
 import { CallProvider } from "@/lib/contexts/CallContext";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: "Ecosphere - Peer-to-Peer Video Calling",
   description: "Secure peer-to-peer audio and video calling application",
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
+        <Analytics />
         <AuthProvider>
           <CallProvider>
             {children}
